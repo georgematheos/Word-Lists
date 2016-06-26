f8db1da5aeaf261ca94302068cd5ed4458415983
George Matheos <gmatheos@exeter.edu>
2016-06-26 12:00:25 -0400

    Fixed bug where a certain error message returned the username field as the string 'req.params.username' rather than the actual value of that property

--- src/lib/verify-token.ts
+++ src/lib/verify-token.ts
@@ -28,7 +28,7 @@ export = function(config) {
 
             // make sure that this is a token for the correct user
             if (decoded.username !== req.params.username) {
-                res.status(401).json({ error: 'Unauthorized', message: 'the token provided is not for the user specified in the url', username: 'req.params.username' });
+                res.status(401).json({ error: 'Unauthorized', message: 'the token provided is not for the user specified in the url', username: req.params.username });
                 return;
             }
 
@@ -38,5 +38,5 @@ export = function(config) {
             // run the next middeware function or route handler
             next();
         });
-    };    
+    };
 }
