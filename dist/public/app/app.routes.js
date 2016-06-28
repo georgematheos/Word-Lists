5b0602a4a6cbd91fb027b88c353244e5a0f63a36
George Matheos <gmatheos@exeter.edu>
2016-06-28 18:20:41 -0400

    first layout and styles for list view

--- dist/public/app/app.routes.js
+++ dist/public/app/app.routes.js
@@ -1,12 +1,14 @@
 "use strict";
 var router_1 = require('@angular/router');
 var home_component_1 = require('./home/home.component');
+var list_component_1 = require('./list/list.component');
 var login_component_1 = require('./login/login.component');
 var signup_component_1 = require("./signup/signup.component");
 var unrecognized_path_component_1 = require('./unrecognized-path/unrecognized-path.component');
 exports.routes = [
     { path: '', redirectTo: '/login', terminal: true },
     { path: 'home/:username', component: home_component_1.HomeComponent },
+    { path: 'list/:username/:title', component: list_component_1.ListComponent },
     { path: 'login', component: login_component_1.LoginComponent },
     { path: 'signup', component: signup_component_1.SignupComponent },
     { path: '**', component: unrecognized_path_component_1.UnrecognizedPathComponent }
